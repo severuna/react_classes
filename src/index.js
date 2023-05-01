572f775c566f1d0457daf547d397d8c705f903f7
Severuna <oxxxyfomenkova@gmail.com>
2023-05-01 21:50:47 +0300

    set main markup & styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+class Timer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {  
+      second: '00',
+      min: '00',
+      hour: '00'
+    }
+  }
+
+  setStart () {
+    console.log('start')
+  }
+
+  setPause () {
+    console.log('pause')
+  }
+
+  render() {
+    return (
+      <div className='timer column'>
+        <h1 className='title'>Homework 18. ReactJS. Классові компоненти. Стан та життєвий цикл</h1>
+        <div className='btns row'>
+          <button className='btn' onClick={() => this.setStart()}>Start</button>
+          <button className='btn' onClick={() => this.setPause()}>Pause</button>
+        </div>
+        <div className='screen row'>
+          <div className='screen-field column'>
+            <p className='time'>{this.state.hour}</p>
+            <p className='value'>hour</p>
+          </div>
+          <div className='screen-field column'>
+            <p className='time'>{this.state.min}</p>
+            <p className='value'>minute</p>
+          </div>
+          <div className='screen-field column'>
+            <p className='time'>{this.state.second}</p>
+            <p className='value'>second</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Homework 18. ReactJS. Классові компоненти Стан та життєвий цикл</h1>
+  <Timer />
 );
